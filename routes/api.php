38158e6c64984596312759a5affa4186b9e09b52
Ngabjago <86274500+AnnasIsmail@users.noreply.github.com>
2022-08-01 14:12:57 +0700

    Test Make Api

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Helpers\ApiFormatter;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('/coba', function () {
+    return ApiFormatter::createApi(200, 'success', [1,2,3]);
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
